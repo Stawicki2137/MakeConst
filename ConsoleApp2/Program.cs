95a32614a26479d4d658eae8570ec3259d65c949
MateuszS <mateusz.stawicki212@gmail.com>
2025-11-03 12:20:46 +0100

    Code fix implemented

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //some comments
+            const int x = 5;
+            Console.WriteLine(x);
+            const int i = 1;
+            const int j = 2;
+            const int k = i + j;
+
+        }
+    }
+}
